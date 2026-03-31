28c47272d8ca59c1322836acd0a3bfe0090e28c6
M6FGR <azooz77899@gmail.com>
2026-04-01 00:38:35 +0300

    cleaned up the process, check [WeaponCapabilityRegistry]

--- src/main/java/M6FGR/epic_api/mixins/epicfight/WeaponTypeReloadListenerMixin.java
+++ src/main/java/M6FGR/epic_api/mixins/epicfight/WeaponTypeReloadListenerMixin.java
@@ -1,25 +1,17 @@
 package M6FGR.epic_api.mixins.epicfight;
 
 import M6FGR.epic_api.api.registry.WeaponCapabilityRegistry;
-import M6FGR.epic_api.main.EpicAPI;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.ListTag;
 import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import yesman.epicfight.api.animation.AnimationManager;
-import yesman.epicfight.api.animation.types.AttackAnimation;
-import yesman.epicfight.world.capabilities.item.Style;
 import yesman.epicfight.world.capabilities.item.WeaponCapability;
-import yesman.epicfight.world.capabilities.item.WeaponCategory;
 import yesman.epicfight.world.capabilities.item.WeaponTypeReloadListener;
+// deprecated!
 import yesman.epicfight.world.capabilities.provider.ExtraEntryProvider;
-
-import java.util.ArrayList;
-import java.util.List;
-// make it datapack friendly too!
+// make HeavyAttack datapack friendly too!
 @Mixin(value = WeaponTypeReloadListener.class, remap = false)
 @SuppressWarnings("removal")
 public class WeaponTypeReloadListenerMixin {
@@ -30,34 +22,6 @@ public class WeaponTypeReloadListenerMixin {
             remap = false
     )
     private static void injectHeavyCombos(ResourceLocation rl, CompoundTag tag, ExtraEntryProvider extraEntryProvider, CallbackInfoReturnable<WeaponCapability.Builder> cir) {
-        if (tag.contains("heavy_combos")) {
-            try {
-                String categoryStr = tag.getString("category");
-                WeaponCategory category = WeaponCategory.ENUM_MANAGER.getOrThrow(categoryStr);
-                CompoundTag heavyCombosTag = tag.getCompound("heavy_combos");
-
-                for (String key : heavyCombosTag.getAllKeys()) {
-                    Style style = Style.ENUM_MANAGER.getOrThrow(key);
-                    ListTag comboAnimations = heavyCombosTag.getList(key, 8);
-                    List<AnimationManager.AnimationAccessor<? extends AttackAnimation>> anims = new ArrayList<>();
-
-                    for (int i = 0; i < comboAnimations.size(); ++i) {
-                        String animId = comboAnimations.getString(i);
-                        AnimationManager.AnimationAccessor<? extends AttackAnimation> animation =
-                                (extraEntryProvider == null) ? AnimationManager.byKey(animId) : extraEntryProvider.getExtraOrBuiltInAnimation(animId);
-
-                        if (animation != null) {
-                            anims.add(animation);
-                        }
-                    }
-
-                    if (!anims.isEmpty()) {
-                        WeaponCapabilityRegistry.builder().registerHeavyComboFromTag(category, style, anims);
-                    }
-                }
-            } catch (Exception e) {
-                EpicAPI.LOGGER.error("Failed to inject heavy combos for " + rl, e);
-            }
-        }
+        WeaponCapabilityRegistry.builder().registerHeavyComboFromTag(rl, tag, extraEntryProvider);
     }
 }

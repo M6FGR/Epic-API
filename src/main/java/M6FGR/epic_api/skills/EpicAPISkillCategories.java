8df4a9e9604e3e179a7976697da05be39421812c
M6FGR <azooz77899@gmail.com>
2026-03-31 23:04:32 +0300

    added translatable components

--- src/main/java/M6FGR/epic_api/skills/EpicAPISkillCategories.java
+++ src/main/java/M6FGR/epic_api/skills/EpicAPISkillCategories.java
@@ -1,19 +1,22 @@
 package M6FGR.epic_api.skills;
 
+import net.minecraft.network.chat.Component;
 import yesman.epicfight.skill.SkillCategory;
 
 public enum EpicAPISkillCategories implements SkillCategory {
-    HEAVY_ATTACK(false, false, false);
+    HEAVY_ATTACK(false, false, false, "skill.epic_api.category.heavy_attack");
 
     final boolean shouldSave;
     final boolean synchronize;
     final boolean learnable;
+    final Component translationKey;
     final int id;
 
-    EpicAPISkillCategories(boolean shouldSave, boolean synchronize, boolean learnable){
+    EpicAPISkillCategories(boolean shouldSave, boolean synchronize, boolean learnable, String translationKey) {
         this.shouldSave = shouldSave;
         this.synchronize = synchronize;
         this.learnable = learnable;
+        this.translationKey = Component.translatable(translationKey);
         this.id = SkillCategory.ENUM_MANAGER.assign(this);
     }
 
@@ -30,6 +33,11 @@ public enum EpicAPISkillCategories implements SkillCategory {
     @Override
     public boolean learnable() {
         return this.learnable;
+    }
+
+    @Override
+    public Component getTranslationKey() {
+        return this.translationKey;
     }
 
     @Override

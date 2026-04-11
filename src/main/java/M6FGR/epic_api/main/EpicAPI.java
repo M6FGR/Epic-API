fb9ef769f2fb2d5ae9c8a7f54977a2483d925900
M6FGR <azooz77899@gmail.com>
2026-04-11 17:01:07 +0300

    Fixed issue events won't fire for other addons

--- src/main/java/M6FGR/epic_api/main/EpicAPI.java
+++ src/main/java/M6FGR/epic_api/main/EpicAPI.java
@@ -1,10 +1,12 @@
 package M6FGR.epic_api.main;
 
-import M6FGR.epic_api.api.cls.ILoadableClass;
-import M6FGR.epic_api.api.events.ExCapCapabilityRegistryEvent;
+import M6FGR.epic_api.api.builders.epicfight.ArmatureRegistrar;
+import M6FGR.epic_api.api.builders.epicfight.EntityPatchRegistrar;
+import M6FGR.epic_api.api.builders.minecraft.GameRulesRegistrar;
+import M6FGR.epic_api.api.cls.load.ILoadableClass;
+import M6FGR.epic_api.api.events.IEventHook;
+import M6FGR.epic_api.api.events.item.ExCapCapabilityRegistryEventHook;
 import M6FGR.epic_api.api.input.EpicAPIIntputAction;
-import M6FGR.epic_api.api.registry.ArmatureRegistry;
-import M6FGR.epic_api.api.registry.EntityPatchRegistry;
 import M6FGR.epic_api.gameassets.EpicAPIKeyMappings;
 import M6FGR.epic_api.gameassets.EpicAPISkillDataKeys;
 import M6FGR.epic_api.gameassets.EpicAPISkills;
@@ -12,6 +14,8 @@ import M6FGR.epic_api.skills.EpicAPISkillCategories;
 import M6FGR.epic_api.skills.EpicAPISkillSlots;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.loading.FMLEnvironment;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import yesman.epicfight.api.client.input.action.InputAction;
@@ -21,24 +25,37 @@ import yesman.epicfight.skill.SkillSlot;
 @Mod(EpicAPI.MODID)
 public class EpicAPI {
     public static final String MODID = "epic_api";
-    public static final Logger LOGGER = LogManager.getLogger();
+    public static final Logger LOGGER = LogManager.getLogger("EpicAPI");
 
     public EpicAPI(IEventBus modBus) {
+        // never load events via ILoadableClass!, it will only fire for me
+        modBus.addListener(this::commonEvents);
+
         ILoadableClass.loadClasses(modBus,
-                // API registry
-                ArmatureRegistry.class,
-                EntityPatchRegistry.class,
+                // EpicFight registrars
+                ArmatureRegistrar.class,
+                EntityPatchRegistrar.class,
+                // Minecraft Registrars
+                GameRulesRegistrar.class,
                 // Assets registry
                 EpicAPISkills.class,
                 EpicAPISkillDataKeys.class,
-                EpicAPIKeyMappings.class,
-                // Events Registry
-                ExCapCapabilityRegistryEvent.class
+                EpicAPIKeyMappings.class
         );
         // EpicFight Extensible Enums Registry
         SkillSlot.ENUM_MANAGER.registerEnumCls(MODID, EpicAPISkillSlots.class);
         SkillCategory.ENUM_MANAGER.registerEnumCls(MODID, EpicAPISkillCategories.class);
         InputAction.ENUM_MANAGER.registerEnumCls(MODID, EpicAPIIntputAction.class);
+    }
+
+    public static boolean isDeveloper() {
+        return !FMLEnvironment.production;
+    }
+
+    private void commonEvents(FMLCommonSetupEvent event) {
+        IEventHook.fire(
+                ExCapCapabilityRegistryEventHook.class
+        );
     }
 
 

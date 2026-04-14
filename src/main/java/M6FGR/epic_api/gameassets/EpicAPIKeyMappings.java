6bf2def2c829519029125ea9558c098b55f30789
M6FGR <azooz77899@gmail.com>
2026-04-14 20:46:25 +0300

    Fixed issue causes a crash in newest epic fight (21.16.3)

--- src/main/java/M6FGR/epic_api/gameassets/EpicAPIKeyMappings.java
+++ src/main/java/M6FGR/epic_api/gameassets/EpicAPIKeyMappings.java
@@ -1,27 +1,25 @@
 package M6FGR.epic_api.gameassets;
 
-import M6FGR.epic_api.api.cls.ILoadableClass;
+import M6FGR.epic_api.cls.ILoadableClass;
 import M6FGR.epic_api.main.EpicAPI;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 import yesman.epicfight.client.input.CombatKeyMapping;
+import yesman.epicfight.platform.client.ClientModPlatformProvider;
 
 public class EpicAPIKeyMappings implements ILoadableClass {
-    public static final KeyMapping HEAVY_ATTACK;
+    public static final KeyMapping HEAVY_ATTACK = new CombatKeyMapping(
+            "key.epic_api.heavy_attack",
+            InputConstants.Type.KEYSYM,
+            82,
+            "key.epicfight.combat"
+    );
 
-    private void registerKeys(RegisterKeyMappingsEvent event) {
-        event.register(HEAVY_ATTACK);
-        EpicAPI.LOGGER.info("Registered all key mappings!");
-    }
-    static {
-        HEAVY_ATTACK = new CombatKeyMapping("key.epic_api.heavy_attack", InputConstants.Type.KEYSYM, 82, "key.epicfight.combat");
-    }
 
-    // can be passed even as a subscribe event
     @Override
-    public void onModConstructor(IEventBus bus) {
-        bus.addListener(this::registerKeys);
+    public void onModClientConstructor(IEventBus modBus) {
+        ClientModPlatformProvider.get().keyMappingRegistrar().registerKeyMapping(HEAVY_ATTACK);
+        EpicAPI.LOGGER.info("registered keymappings");
     }
 }

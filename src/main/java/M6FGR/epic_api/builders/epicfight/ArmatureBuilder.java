50b8ec33da143644a85ede83f690458ec01b3733
M6FGR <azooz77899@gmail.com>
2026-04-26 15:54:43 +0300

    Changed the Name and added internal methods

--- src/main/java/M6FGR/epic_api/builders/epicfight/ArmatureBuilder.java
+++ src/main/java/M6FGR/epic_api/builders/epicfight/ArmatureBuilder.java
@@ -3,87 +3,86 @@ package M6FGR.epic_api.builders.epicfight;
 import M6FGR.epic_api.models.armature.NonHumanoidArmature;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
-import org.jetbrains.annotations.ApiStatus.Experimental;
+import org.jetbrains.annotations.ApiStatus.Internal;
 import yesman.epicfight.api.animation.Joint;
-import yesman.epicfight.api.asset.AssetAccessor;
 import yesman.epicfight.api.model.Armature;
 import yesman.epicfight.gameasset.Armatures;
+import yesman.epicfight.gameasset.Armatures.ArmatureAccessor;
 import yesman.epicfight.gameasset.Armatures.ArmatureContructor;
 import yesman.epicfight.model.armature.HumanoidArmature;
 
 import javax.annotation.Nullable;
 import java.util.Map;
 
-@Experimental
-public class ArmatureRegistrar {
-    private record ArmatureMap(EntityType<?> entityType, AssetAccessor<? extends Armature> armature) {}
+public class ArmatureBuilder {
 
-
-    private ArmatureRegistrar() {}
+    private ArmatureBuilder() {}
 
     // Automatically puts the armature to the entity, no need for events, and can accept 3 arguments:
     public static <AR extends Armature> Armatures.ArmatureAccessor<AR> newEntityArmature(EntityType<?> entity, String parsedPath, Armatures.ArmatureContructor<AR> contructor) {
-        Armatures.ArmatureAccessor<AR> accessor = newArmature(parsedPath, contructor);
-        Armatures.registerEntityTypeArmature(entity, accessor);
-        return accessor;
+        ResourceLocation parsed = ResourceLocation.parse(parsedPath);
+        return newEntityArmatureI(entity, parsed.getNamespace(), parsed.getPath(), contructor);
     }
 
     public static <AR extends Armature> Armatures.ArmatureAccessor<AR> newEntityArmature(EntityType<?> entity, ResourceLocation location, Armatures.ArmatureContructor<AR> contructor) {
-        Armatures.ArmatureAccessor<AR> accessor = newArmature(location, contructor);
-        Armatures.registerEntityTypeArmature(entity, accessor);
-        return accessor;
+        return newEntityArmatureI(entity, location.getNamespace(), location.getPath(), contructor);
     }
 
     public static <AR extends Armature> Armatures.ArmatureAccessor<AR> newEntityArmature(EntityType<?> entity, String modid, String path, Armatures.ArmatureContructor<AR> contructor) {
-        Armatures.ArmatureAccessor<AR> accessor = newArmature(modid, path, contructor);
-        Armatures.registerEntityTypeArmature(entity, accessor);
-        return accessor;
+        return newEntityArmatureI(entity, modid, path, contructor);
     }
 
     public static <AR extends Armature> Armatures.ArmatureAccessor<AR> newEntityArmature(EntityType<?> entity, String parsedPath, ArmatureType type) {
-        Armatures.ArmatureAccessor<AR> accessor = newArmature(parsedPath, type::apply);
-        Armatures.registerEntityTypeArmature(entity, accessor);
-        return accessor;
+        ResourceLocation parsed = ResourceLocation.parse(parsedPath);
+        return newEntityArmatureI(entity, parsed.getNamespace(), parsed.getPath(), type::apply);
     }
 
     public static <AR extends Armature> Armatures.ArmatureAccessor<AR> newEntityArmature(EntityType<?> entity, ResourceLocation location, ArmatureType type) {
-        Armatures.ArmatureAccessor<AR> accessor = newArmature(location, type::apply);
-        Armatures.registerEntityTypeArmature(entity, accessor);
-        return accessor;
+        return newEntityArmatureI(entity, location.getNamespace(), location.getPath(), type::apply);
     }
 
     public static <AR extends Armature> Armatures.ArmatureAccessor<AR> newEntityArmature(EntityType<?> entity, String modid, String path, ArmatureType type) {
-        Armatures.ArmatureAccessor<AR> accessor = newArmature(modid, path, type::apply);
-        Armatures.registerEntityTypeArmature(entity, accessor);
-        return accessor;
+        return newEntityArmatureI(entity, modid, path, type::apply);
     }
 
     // needs to be posted via an event (FMLCommonSetupEvent)
 
     public static <AR extends Armature> Armatures.ArmatureAccessor<AR> newArmature(String parsedPath, Armatures.ArmatureContructor<AR> contructor) {
-        ResourceLocation path = ResourceLocation.parse(parsedPath);
-        return Armatures.ArmatureAccessor.create(path.getNamespace(), path.getPath(), contructor);
+        ResourceLocation parsed = ResourceLocation.parse(parsedPath);
+        return newArmatureI(parsed.getNamespace(), parsed.getPath(), contructor);
     }
 
     public static <AR extends Armature> Armatures.ArmatureAccessor<AR> newArmature(ResourceLocation location, Armatures.ArmatureContructor<AR> contructor) {
-        return Armatures.ArmatureAccessor.create(location.getNamespace(), location.getPath(), contructor);
+        return newArmatureI(location.getNamespace(), location.getPath(), contructor);
     }
 
     public static <AR extends Armature> Armatures.ArmatureAccessor<AR> newArmature(String modid, String path, Armatures.ArmatureContructor<AR> contructor) {
-        return Armatures.ArmatureAccessor.create(modid, path, contructor);
+        return newArmatureI(modid, path, contructor);
     }
 
     public static <AR extends Armature> Armatures.ArmatureAccessor<AR> newArmature(String parsedPath, ArmatureType type) {
-        ResourceLocation path = ResourceLocation.parse(parsedPath);
-        return Armatures.ArmatureAccessor.create(path.getNamespace(), path.getPath(), type::apply);
+        ResourceLocation parsed = ResourceLocation.parse(parsedPath);
+        return newArmatureI(parsed.getNamespace(), parsed.getPath(), type::apply);
     }
 
     public static <AR extends Armature> Armatures.ArmatureAccessor<AR> newArmature(ResourceLocation location, ArmatureType type) {
-        return Armatures.ArmatureAccessor.create(location.getNamespace(), location.getPath(), type::apply);
+        return newArmatureI(location.getNamespace(), location.getPath(), type::apply);
     }
 
     public static <AR extends Armature> Armatures.ArmatureAccessor<AR> newArmature(String modid, String path, ArmatureType type) {
-        return Armatures.ArmatureAccessor.create(modid, path, type::apply);
+        return newArmatureI(modid, path, type::apply);
+    }
+
+    @Internal
+    private static <AR extends Armature> ArmatureAccessor<AR> newArmatureI(String modid, String path, ArmatureContructor<AR> contructor) {
+        return ArmatureAccessor.create(modid, path, contructor);
+    }
+
+    @Internal
+    private static <AR extends Armature> ArmatureAccessor<AR> newEntityArmatureI(EntityType<?> entity, String modid, String path, ArmatureContructor<AR> contructor) {
+        ArmatureAccessor<AR> accessor = newArmatureI(modid, path, contructor);
+        Armatures.registerEntityTypeArmature(entity, accessor);
+        return accessor;
     }
 
     public enum ArmatureType {
@@ -98,7 +97,7 @@ public class ArmatureRegistrar {
         }
 
         @SuppressWarnings("unchecked")
-        public <AR extends Armature> AR apply(String name, int jointNumber, Joint joint, Map<String, Joint> jointMap) {
+        <AR extends Armature> AR apply(String name, int jointNumber, Joint joint, Map<String, Joint> jointMap) {
             // We cast the result of the invocation to the generic type AR
             return (AR) this.constructor.invoke(name, jointNumber, joint, jointMap);
         }

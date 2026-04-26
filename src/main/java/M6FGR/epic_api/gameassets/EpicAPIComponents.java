9fa8cd419e3cf4100e2c714bd3611e66c7a4e321
M6FGR <azooz77899@gmail.com>
2026-04-26 16:36:09 +0300

    New class for ease of code readability

---
+++ src/main/java/M6FGR/epic_api/gameassets/EpicAPIComponents.java
@@ -0,0 +1,7 @@
+package M6FGR.epic_api.gameassets;
+
+import net.minecraft.network.chat.Component;
+// Stores translation keys, names, any kind of text-asset ingame
+public class EpicAPIComponents {
+    public static final String HEAVY_ATTACK = Component.translatable("key.epic_api.heavy_attack").getString();
+}

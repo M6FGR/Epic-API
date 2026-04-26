006d901108e81cf986f056697ef07a56f6a365e4
M6FGR <azooz77899@gmail.com>
2026-04-26 15:37:04 +0300

    First push of .animation.types. Package Classes

---
+++ src/main/java/M6FGR/epic_api/animation/types/SimpleMovementAnimation.java
@@ -0,0 +1,72 @@
+package M6FGR.epic_api.animation.types;
+
+import yesman.epicfight.api.animation.AnimationManager;
+import yesman.epicfight.api.animation.types.DynamicAnimation;
+import yesman.epicfight.api.asset.AssetAccessor;
+import yesman.epicfight.api.client.animation.Layer;
+import yesman.epicfight.api.client.animation.property.JointMaskEntry;
+import yesman.epicfight.api.client.camera.EpicFightCameraAPI;
+import yesman.epicfight.api.model.Armature;
+import yesman.epicfight.world.capabilities.entitypatch.LivingEntityPatch;
+// same as SimpleStaticAnimation, but with custom playspeed and advanced reverse play
+public class SimpleMovementAnimation extends SimpleStaticAnimation {
+    protected float movementSpeed;
+
+    public SimpleMovementAnimation(boolean isRepeat, float movementSpeed, AnimationManager.AnimationAccessor<? extends SimpleStaticAnimation> accessor, AssetAccessor<? extends Armature> armature) {
+        super(isRepeat, accessor, armature);
+        this.movementSpeed = movementSpeed;
+    }
+
+    public SimpleMovementAnimation(boolean isRepeat, AnimationManager.AnimationAccessor<? extends SimpleStaticAnimation> accessor, AssetAccessor<? extends Armature> armature) {
+        super(isRepeat, accessor, armature);
+    }
+
+    public SimpleMovementAnimation(float transitionTime, boolean isRepeat, float movementSpeed, AnimationManager.AnimationAccessor<? extends SimpleStaticAnimation> accessor, AssetAccessor<? extends Armature> armature) {
+        super(transitionTime, isRepeat, accessor, armature);
+        this.movementSpeed = movementSpeed;
+    }
+
+    @Override
+    public SimpleMovementAnimation withPriority(Layer.Priority priority) {
+        super.withPriority(priority);
+        return this;
+    }
+    @Override
+    public SimpleMovementAnimation withLayer(Layer.LayerType layer) {
+        super.withLayer(layer);
+        return this;
+    }
+    @Override
+    public SimpleMovementAnimation withJointMask(JointMasks masks) {
+        super.withJointMask(masks);
+        return this;
+    }
+    @Override
+    public SimpleMovementAnimation withJointMask(JointMaskEntry mask) {
+        super.withJointMask(mask);
+        return this;
+    }
+
+    @Override
+    public float getPlaySpeed(LivingEntityPatch<?> entitypatch, DynamicAnimation animation) {
+        if (animation.isLinkAnimation()) {
+            return 1.0F;
+        } else {
+            float i = 1.0F;
+            if (Math.abs(entitypatch.getOriginal().walkAnimation.speed() - entitypatch.getOriginal().walkAnimation.speed(1.0F)) < 0.007F) {
+                i *= entitypatch.getOriginal().walkAnimation.speed() * this.movementSpeed;
+            }
+
+            return i;
+        }
+    }
+
+    @Override
+    public boolean canBePlayedReverse() {
+        // ONLY plays backwards if the player was actually looking back, not pressing S
+        if (EpicFightCameraAPI.getInstance().isTPSMode()) {
+            return EpicFightCameraAPI.getInstance().isLockingOnTarget() || EpicFightCameraAPI.getInstance().isLerpingFpv() || EpicFightCameraAPI.getInstance().isZooming();
+        }
+        return true;
+    }
+}
---
+++ src/main/java/M6FGR/epic_api/animation/types/SimpleStaticAnimation.java
@@ -0,0 +1,110 @@
+package M6FGR.epic_api.animation.types;
+
+import yesman.epicfight.api.animation.AnimationManager;
+import yesman.epicfight.api.animation.Joint;
+import yesman.epicfight.api.animation.types.StaticAnimation;
+import yesman.epicfight.api.asset.AssetAccessor;
+import yesman.epicfight.api.client.animation.Layer;
+import yesman.epicfight.api.client.animation.property.ClientAnimationProperties;
+import yesman.epicfight.api.client.animation.property.JointMask;
+import yesman.epicfight.api.client.animation.property.JointMaskEntry;
+import yesman.epicfight.api.model.Armature;
+import yesman.epicfight.gameasset.Armatures;
+
+import java.util.Arrays;
+import java.util.function.Supplier;
+// Can access Layer, Priority, and has its own JoinMask enum, no data for animations needed
+public class SimpleStaticAnimation extends StaticAnimation {
+    public SimpleStaticAnimation(float transitionTime, boolean repeatable, AnimationManager.AnimationAccessor<? extends SimpleStaticAnimation> animation, AssetAccessor<? extends Armature> armature) {
+        super(transitionTime, repeatable, animation, armature);
+    }
+
+    public SimpleStaticAnimation(boolean repeatable, AnimationManager.AnimationAccessor<? extends SimpleStaticAnimation> animation, AssetAccessor<? extends Armature> armature) {
+        super(repeatable, animation, armature);
+    }
+
+    public SimpleStaticAnimation(AnimationManager.AnimationAccessor<? extends SimpleStaticAnimation> animation, AssetAccessor<? extends Armature> armature) {
+        super(true, animation, armature);
+    }
+
+    public SimpleStaticAnimation withPriority(Layer.Priority priority) {
+        this.addProperty(ClientAnimationProperties.PRIORITY, priority);
+        return this;
+    }
+
+    public SimpleStaticAnimation withLayer(Layer.LayerType layer) {
+        this.addProperty(ClientAnimationProperties.LAYER_TYPE, layer);
+        return this;
+    }
+
+    public SimpleStaticAnimation withJointMask(JointMasks masks) {
+        this.addProperty(ClientAnimationProperties.JOINT_MASK, masks.getEntry());
+        return this;
+    }
+    public SimpleStaticAnimation withJointMask(JointMaskEntry mask) {
+        this.addProperty(ClientAnimationProperties.JOINT_MASK, mask);
+        return this;
+    }
+
+    public enum JointMasks {
+        ROOT_UPPER_JOINTS(() -> joints(
+                Armatures.BIPED.get().torso,
+                Armatures.BIPED.get().chest,
+                Armatures.BIPED.get().head,
+                // right arms
+                Armatures.BIPED.get().shoulderR, Armatures.BIPED.get().armR,
+                Armatures.BIPED.get().handR, Armatures.BIPED.get().toolR,
+                // left arms
+                Armatures.BIPED.get().shoulderL, Armatures.BIPED.get().armL,
+                Armatures.BIPED.get().handL, Armatures.BIPED.get().toolL
+        )),
+        LEFT_ARM(() -> joints(
+                Armatures.BIPED.get().shoulderL, Armatures.BIPED.get().armL,
+                Armatures.BIPED.get().handL, Armatures.BIPED.get().toolL
+        )),
+        RIGHT_ARM(() -> joints(
+                Armatures.BIPED.get().shoulderR, Armatures.BIPED.get().armR,
+                Armatures.BIPED.get().handR, Armatures.BIPED.get().toolR
+        )),
+        LEGS(() -> joints(
+                Armatures.BIPED.get().thighR, Armatures.BIPED.get().kneeR,
+                Armatures.BIPED.get().legR, Armatures.BIPED.get().thighL,
+                Armatures.BIPED.get().kneeL, Armatures.BIPED.get().legL
+        )),
+        NONE(JointMasks::joints);
+
+        private final Supplier<Joint[]> jointsSupplier;
+
+        JointMasks(Supplier<Joint[]> jointsSupplier) {
+            this.jointsSupplier = jointsSupplier;
+        }
+
+        /*
+          Internal helper to clean up enum definitions.
+        */
+        private static Joint[] joints(Joint... joints) {
+            return joints;
+        }
+
+        /*
+         * Static factory to create a custom JointMaskEntry.
+         * If no joints are passed, it effectively acts as an "EMPTY" mask.
+         */
+        public static JointMaskEntry createEntry(Joint... joints) {
+            JointMask[] masks = Arrays.stream(joints)
+                    .map(j -> JointMask.of(j.getName()))
+                    .toArray(JointMask[]::new);
+
+            return JointMaskEntry.builder()
+                    .defaultMask(JointMask.JointMaskSet.of(masks))
+                    .create();
+        }
+
+        /*
+         Returns the pre-defined mask entry for this enum constant.
+         */
+        public JointMaskEntry getEntry() {
+            return createEntry(this.jointsSupplier.get());
+        }
+    }
+}

b8e56b6c76a2ceac12d9477679c905072ee4d666
M6FGR <azooz77899@gmail.com>
2026-04-26 16:02:50 +0300

    Added methods for ease of logging and debugging

--- src/main/java/M6FGR/epic_api/main/EpicAPI.java
+++ src/main/java/M6FGR/epic_api/main/EpicAPI.java
@@ -11,6 +11,8 @@ import M6FGR.epic_api.gameassets.EpicAPISkills;
 import M6FGR.epic_api.input.EpicAPIIntputAction;
 import M6FGR.epic_api.skills.EpicAPISkillCategories;
 import M6FGR.epic_api.skills.EpicAPISkillSlots;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -24,11 +26,12 @@ import yesman.epicfight.skill.SkillSlot;
 @Mod(EpicAPI.MODID)
 public class EpicAPI {
     public static final String MODID = "epic_api";
-    public static final Logger LOGGER = LogManager.getLogger("EpicAPI");
+    private static final Logger LOGGER = LogManager.getLogger("EpicAPI");
 
     public EpicAPI(IEventBus modBus) {
         // never load events via ILoadableClass!, it will only fire for Epic-API!
-        modBus.addListener(this::commonEvents);
+        modBus.addListener(this::doCommonEvents);
+
         ILoadableClass.loadClasses(modBus,
                 // Minecraft Builders
                 GameRulesBuilder.class,
@@ -37,23 +40,59 @@ public class EpicAPI {
                 EpicAPISkillDataKeys.class,
                 EpicAPIKeyMappings.class
         );
+
         // EpicFight Extensible Enums Registry
         SkillSlot.ENUM_MANAGER.registerEnumCls(MODID, EpicAPISkillSlots.class);
         SkillCategory.ENUM_MANAGER.registerEnumCls(MODID, EpicAPISkillCategories.class);
         InputAction.ENUM_MANAGER.registerEnumCls(MODID, EpicAPIIntputAction.class);
     }
 
+    // Logger helpers
+    public static void err(String message, Object... args) {
+        LOGGER.error(message, args);
+    }
+
+    public static void warn(String message, Object... args) {
+        LOGGER.warn(message, args);
+    }
+
+    public static void debug(String message, Object... args) {
+        LOGGER.debug(message, args);
+    }
+
+    public static void info(String message, Object... args) {
+        LOGGER.debug(message, args);
+    }
+
+    private static boolean sideIs(Dist dist) {
+        return FMLEnvironment.dist == dist;
+    }
+
+    public static boolean isClient() {
+        return sideIs(Dist.CLIENT);
+    }
+
+    public static boolean isDedicatedServer() {
+        return sideIs(Dist.DEDICATED_SERVER);
+    }
+
     public static boolean isDeveloper() {
         return !FMLEnvironment.production;
     }
 
-    private void commonEvents(FMLCommonSetupEvent event) {
-       event.enqueueWork(() -> {
-           IEventHook.fire(
-                   ExCapCapabilityRegistryEventHook.class,
-                   EntityPatchEventHook.class
-           );
-       });
+    public static ResourceLocation identifier(String path) {
+        return ResourceLocation.fromNamespaceAndPath(MODID, path);
+    }
+
+
+    // events/registries
+    private void doCommonEvents(FMLCommonSetupEvent event) {
+        // these events are fired in the common setup event, so we can fire it here?
+       event.enqueueWork(() ->
+               IEventHook.fire(
+                       ExCapCapabilityRegistryEventHook.class,
+                       EntityPatchEventHook.class
+               ));
     }
 
 

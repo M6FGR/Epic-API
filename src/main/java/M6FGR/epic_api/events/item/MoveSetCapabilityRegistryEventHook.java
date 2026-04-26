31feefc45d289ad005c7566a7dc75aff04a1004d
M6FGR <azooz77899@gmail.com>
2026-04-26 16:33:49 +0300

    renamed from ExCapCapabilityRegistryEventHook.java

--- src/main/java/M6FGR/epic_api/events/item/MoveSetCapabilityRegistryEventHook.java
+++ src/main/java/M6FGR/epic_api/events/item/MoveSetCapabilityRegistryEventHook.java
@@ -1,40 +1,34 @@
 package M6FGR.epic_api.events.item;
 
+import M6FGR.epic_api.builders.epicfight.MoveSetBuilder;
 import M6FGR.epic_api.events.EpicAPIEventHooks;
 import M6FGR.epic_api.events.IEventHook;
-import M6FGR.epic_api.builders.epicfight.MoveSetBuilder;
+import com.google.common.collect.Maps;
 import net.minecraft.resources.ResourceLocation;
 import yesman.epicfight.api.event.EpicFightEventHooks;
 import yesman.epicfight.api.event.Event;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 
-public class ExCapCapabilityRegistryEventHook extends Event implements IEventHook {
-    private final Map<ResourceLocation, MoveSetBuilder> registryMap = new HashMap<>();
-    private ResourceLocation id;
-
+public class MoveSetCapabilityRegistryEventHook extends Event implements IEventHook {
+    private final Map<ResourceLocation, MoveSetBuilder> registryMap = Maps.newHashMap();
 
     public void register(ResourceLocation id, MoveSetBuilder registry) {
+        // add a double-registry blocker
+        if (this.registryMap.containsKey(id)) {
+            throw new IllegalArgumentException("MoveSet ID: " + id + "is already registered!");
+        }
         registry.build(id);
-        this.id = id;
         this.registryMap.put(id, registry);
-    }
-
-    public Map<ResourceLocation, MoveSetBuilder> getRegistryMap() {
-        return Collections.unmodifiableMap(this.registryMap);
     }
 
     @Override
     public void post() {
-        EpicAPIEventHooks.Registry.MOVE_SET_CAPABILITY.post(this);
-
         EpicFightEventHooks.Registry.EX_CAP_DATA_CREATION.registerEvent(event ->
                 this.registryMap.values().forEach(reg -> event.addData(reg.getDataEntry())));
 
         EpicFightEventHooks.Registry.EX_CAP_MOVESET_REGISTRY.registerEvent(event ->
-                this.registryMap.values().forEach(reg -> event.addMoveSet(reg.build(this.id))));
+                this.registryMap.forEach((id, reg) -> event.addMoveSet(reg.build(id))));
 
         EpicFightEventHooks.Registry.EX_CAP_BUILDER_CREATION.registerEvent(event ->
                 this.registryMap.values().forEach(reg -> event.addBuilder(reg.getBuilderEntry())));
@@ -49,5 +43,11 @@ public class ExCapCapabilityRegistryEventHook extends Event implements IEventHoo
             // and map it to a provider that builds the WeaponCapability
             event.getTypeEntry().put(id, item -> reg.getWeaponCapability());
         }));
+        EpicAPIEventHooks.Registry.MOVE_SET_CAPABILITY.post(this);
+    }
+
+    @Override
+    public void postClient() {
+        // ignored, nothing in this event is related to client-side events?
     }
 }

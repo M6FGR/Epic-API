81a64796827a16d7bc2a68f6b9a15fd1a7819672
M6FGR <azooz77899@gmail.com>
2026-04-26 16:34:46 +0300

    added a new enum and used internal methods

--- src/main/java/M6FGR/epic_api/builders/epicfight/MeshBuilder.java
+++ src/main/java/M6FGR/epic_api/builders/epicfight/MeshBuilder.java
@@ -1,16 +1,19 @@
 package M6FGR.epic_api.builders.epicfight;
 
 import net.minecraft.resources.ResourceLocation;
-import org.jetbrains.annotations.ApiStatus.Experimental;
+import org.jetbrains.annotations.ApiStatus.Internal;
 import yesman.epicfight.api.asset.JsonAssetLoader;
-import yesman.epicfight.api.client.model.*;
+import yesman.epicfight.api.client.model.ClassicMesh;
+import yesman.epicfight.api.client.model.Mesh;
+import yesman.epicfight.api.client.model.Meshes;
 import yesman.epicfight.api.client.model.Meshes.MeshContructor;
+import yesman.epicfight.api.client.model.SkinnedMesh;
 import yesman.epicfight.api.client.model.SkinnedMesh.SkinnedMeshPart;
+import yesman.epicfight.api.client.model.VertexBuilder;
 import yesman.epicfight.client.mesh.HumanoidMesh;
 
 import java.util.function.Function;
 
-@Experimental
 public class MeshBuilder {
     private MeshBuilder() {}
 
@@ -28,14 +31,18 @@ public class MeshBuilder {
         return toAccessor(modid, path, type);
     }
 
+    // Avoid usage, Use MeshAccessor#create instead
+    @Internal
     public static <ME extends SkinnedMesh> Meshes.MeshAccessor<ME> newMesh(String modid, String path, MeshContructor<SkinnedMeshPart, VertexBuilder, ME> constructor) {
         return Meshes.MeshAccessor.create(modid, path, loader -> loader.loadSkinnedMesh(constructor));
     }
 
+    @Internal
     public static <ME extends SkinnedMesh> Meshes.MeshAccessor<ME> newMesh(ResourceLocation location, MeshContructor<SkinnedMeshPart, VertexBuilder, ME> constructor) {
         return Meshes.MeshAccessor.create(location.getNamespace(), location.getPath(), loader -> loader.loadSkinnedMesh(constructor));
     }
 
+    @Internal
     public static <ME extends SkinnedMesh> Meshes.MeshAccessor<ME> newMesh(String parsedLocation, MeshContructor<SkinnedMeshPart, VertexBuilder, ME> constructor) {
         ResourceLocation parsed = ResourceLocation.parse(parsedLocation);
         return Meshes.MeshAccessor.create(parsed.getNamespace(), parsed.getPath(), loader -> loader.loadSkinnedMesh(constructor));
@@ -49,6 +56,7 @@ public class MeshBuilder {
         SKINNED_MESH(loader -> loader.loadSkinnedMesh(SkinnedMesh::new)),
         HUMANOID_MESH(loader -> loader.loadSkinnedMesh(HumanoidMesh::new)),
         COMPOSITE_MESH(JsonAssetLoader::loadCompositeMesh),
+        MESH_LOADER(JsonAssetLoader::loadMesh),
         CLASSIC_MESH(loader -> loader.loadClassicMesh(ClassicMesh::new));
 
         private Function<JsonAssetLoader, Mesh> factory;
@@ -57,14 +65,16 @@ public class MeshBuilder {
             this.factory = factory;
         }
 
+        public static MeshType of(MeshContructor<SkinnedMeshPart, VertexBuilder, SkinnedMesh> meshContructor) {
+            SKINNED_MESH.factory = loader -> loader.loadSkinnedMesh(meshContructor);
+            return SKINNED_MESH;
+        }
+
+
         @SuppressWarnings("unchecked")
         public <ME extends Mesh> ME load(JsonAssetLoader loader) {
             return (ME) factory.apply(loader);
         }
 
-        public static MeshType of(Function<JsonAssetLoader, Mesh> meshConstructor) {
-            SKINNED_MESH.factory = meshConstructor;
-            return SKINNED_MESH;
-        }
     }
 }

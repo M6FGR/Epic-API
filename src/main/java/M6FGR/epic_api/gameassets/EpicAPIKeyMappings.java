4ee73987048047037852187e437d21146e7bad1a
M6FGR <azooz77899@gmail.com>
2026-04-26 16:18:47 +0300

    Use the external Class (KeyMappingsBuilder)

--- src/main/java/M6FGR/epic_api/gameassets/EpicAPIKeyMappings.java
+++ src/main/java/M6FGR/epic_api/gameassets/EpicAPIKeyMappings.java
@@ -1,25 +1,44 @@
 package M6FGR.epic_api.gameassets;
 
+import M6FGR.epic_api.builders.minecraft.KeyMappingsBuilder;
+import M6FGR.epic_api.builders.minecraft.KeyMappingsBuilder.InputType;
+import M6FGR.epic_api.builders.minecraft.KeyMappingsBuilder.KeyCategory;
 import M6FGR.epic_api.cls.ILoadableClass;
+import M6FGR.epic_api.input.KeyCodes;
 import M6FGR.epic_api.main.EpicAPI;
-import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
 import net.neoforged.bus.api.IEventBus;
-import yesman.epicfight.client.input.CombatKeyMapping;
-import yesman.epicfight.platform.client.ClientModPlatformProvider;
+import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EpicAPIKeyMappings implements ILoadableClass {
-    public static final KeyMapping HEAVY_ATTACK = new CombatKeyMapping(
-            "key.epic_api.heavy_attack",
-            InputConstants.Type.KEYSYM,
-            82,
-            "key.epicfight.combat"
+    private static final List<KeyMapping> KEY_MAPPINGS = new ArrayList<>();
+    public static KeyMapping HEAVY_ATTACK = KeyMappingsBuilder.newCombatKeyMapping(
+            EpicAPIComponents.HEAVY_ATTACK,
+            InputType.MOUSE,
+            KeyCodes.KEY_X,
+            KeyCategory.EPICFIGHT_COMBAT
     );
 
+    static {
+        KEY_MAPPINGS.add(HEAVY_ATTACK);
+    }
 
     @Override
     public void onModClientConstructor(IEventBus modBus) {
-        ClientModPlatformProvider.get().keyMappingRegistrar().registerKeyMapping(HEAVY_ATTACK);
-        EpicAPI.LOGGER.info("registered keymappings");
+        modBus.addListener(this::onKeysRegistry);
+        EpicAPI.debug("Registered EpicAPI Keymappings.");
     }
+
+
+
+    private void onKeysRegistry(RegisterKeyMappingsEvent event) {
+        for (KeyMapping keyMapping : KEY_MAPPINGS) {
+            event.register(keyMapping);
+        }
+    }
+
+
 }

8feb13e6e8894dbf712d3b18c81ca61b727d2d0d
M6FGR <azooz77899@gmail.com>
2026-06-03 14:26:57 +0300

    Revert "Updated to Epic Fight 21.17.3!"
    This reverts commit 28dde9b0eb649e077f78115f9dfa11d7c672aa17.

--- src/main/java/M6FGR/epic_api/cls/ILoadableClass.java
+++ src/main/java/M6FGR/epic_api/cls/ILoadableClass.java
@@ -98,7 +98,6 @@ public interface ILoadableClass {
             } catch (NoSuchMethodException noCons) {
                 EpicAPI.err("Failed to load Class [{}], no public constructor!", loadableClass.getSimpleName());
                 noCons.printStackTrace();
-
             } catch (Exception e) {
                 EpicAPI.err("Failed to load Class [{}], {}", loadableClass.getName(), e);
                 e.printStackTrace();
